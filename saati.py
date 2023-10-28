4abfc10cc1a7eacbe905c0d4ecae124729e707aa
NikitaZamiatin <nikitvzamatin16@gmail.com>
2023-10-29 00:42:00 +0500

    Вычисления
    Добавил вычисления весовых коэффициентов и округлил их

--- saati.py
+++ saati.py
@@ -4,7 +4,10 @@ matrix = [[1] * n for k in range(n)]
 
 for i in range(n):
     for j in range(i+1, n):
-        value = float(input(f"Введите значение попарного сравнения между критерием {i+1} и критерием {j+1}: "))
-        matrix[i][j] = value
-        matrix[j][i] = 1/value
+        pair_weight = float(input(f"Введите значение попарного сравнения между критерием {i+1} и критерием {j+1}: "))
+        matrix[i][j] = pair_weight
+        matrix[j][i] = 1/pair_weight
 
+weights = [sum(line)/n for line in matrix]
+
+rounded_weights = [round(weight, 2) for weight in weights]
